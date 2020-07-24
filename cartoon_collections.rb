fb04c6bc36287a8bf86aefd19c97ca8e754fb074
Shema Bryan <shema.bryan@jjay.cuny.edu>
2020-07-24 13:24:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,15 +1,11 @@
 def greet_characters(array)
   array.each do |element|
   puts "Hello #{element}!"
-  end 
+  end
 end
-# Use `each` to enumerate over the provided array
-  #
-  # Print a custom greeting for each element
-end
+# Use `each` to enumerate over the provided arra
 
-def list_dwarves(array)
-  # Use `each_with_index` to enumerate over the provided array
+def list_dwarves(array) i
+  
   #
-  # Print a numbered list of each element
-end
+  # Print a numbered list of each elementend
