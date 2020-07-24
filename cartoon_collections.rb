fa868ca71dd14f54525dcbbd690c25f4e6d77e7a
Shema Bryan <shema.bryan@jjay.cuny.edu>
2020-07-24 13:24:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,6 +6,9 @@ end
 # Use `each` to enumerate over the provided arra
 
 def list_dwarves(array) i
-  
+  array.each_with_index do |element, index|
+  puts "#{index + 1}. #{element}"
+end
+end
   #
   # Print a numbered list of each elementend
